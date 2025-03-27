d7492afc7deb0a4cd7d5b1b1a3a3b0d560beda0c
yanfosah17 <yanfosah@icloud.com>
2025-03-27 01:46:31 -0400

    Failover when API is down

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,12 @@
 class TransactionsController < ApplicationController
   def index
-    FetchTransactionsService.call
+    begin
+      FetchTransactionsService.call
+      @api_available = true
+    rescue StandardError
+      @api_available = false
+    end
+
     @transactions = Transaction.order(height: :desc)
   end
 end
