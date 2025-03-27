b6b5d29a4cdd342e52b1c216c3a25f8a53a99c63
yanfosah17 <yanfosah@icloud.com>
2025-03-27 01:55:42 -0400

    add controller test

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
+require "webmock/minitest"
 
 class TransactionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    WebMock.disable_net_connect!(allow_localhost: true)
+  end
+
   test "should get index" do
-    get transactions_index_url
+    # Stub the API response using WebMock
+    stub_request(:get, "https://4816b0d3-d97d-47c4-a02c-298a5081c0f9.mock.pstmn.io/near/transactions?api_key=SECRET_API_KEY")
+      .to_return(status: 200, body: [].to_json, headers: {})
+    
+    get root_url
     assert_response :success
   end
 end
